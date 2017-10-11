a378c557654a7c083c9252b18dea15ca6c338039
npetranek <32692996+npetranek@users.noreply.github.com>
2017-10-11 00:08:46 -0400

    is it up edited to my parameters
    i removed the default parameters on a few things and replaced them with ones that link back to me so that i can get a sense on if i made the changes correctly

--- isitup.php
+++ isitup.php
@@ -29,7 +29,7 @@ $text = $_POST['text'];
 $token = $_POST['token'];
 
 # Check the token and make sure the request is from our team 
-if($token != 'vnLfaOlI7natbpU5tKQBm5dQ'){ #replace this with the token from your slash command configuration page
+if($token != 'f3T5YBTgmDfA41rtNXdJ1wWa'){ #replace this with the token from your slash command configuration page
   $msg = "The token for the slash command doesn't match. Check your script.";
   die($msg);
   echo $msg;
@@ -38,7 +38,7 @@ if($token != 'vnLfaOlI7natbpU5tKQBm5dQ'){ #replace this with the token from your
 
 # isitup.org doesn't require you to use API keys, but they do require that any automated script send in a user agent string.
 # You can keep this one, or update it to something that makes more sense for you
-$user_agent = "IsitupForSlack/1.0 (https://github.com/mccreath/istiupforslack; mccreath@gmail.com)";
+$user_agent = "IsitupForSlack/1.0 (https://github.com/npetranek/slashcommandsslack; npetranek95@gmail.com)";
 
 # We're just taking the text exactly as it's typed by the user. If it's not a valid domain, isitup.org will respond with a `3`.
 # We want to get the JSON version back (you can also get plain text).
